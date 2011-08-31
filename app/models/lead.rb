4ae730ebc51a37666caf9ebc5096f8b42708d9c8
Clément Renaud <clement.renaud@gmail.com>
2011-08-31 14:20:28 +0800

    fix home featured unique file name

--- app/models/lead.rb
+++ app/models/lead.rb
@@ -9,12 +9,16 @@ class Lead < ActiveRecord::Base
                     :path => ":rails_root/public/uploads/leads/:id/:basename.:extension",
                     :url => "/uploads/leads/:id/:basename.:extension"
   
-  before_create :unique_file_name
   
+  validates_attachment_size :featimage, :less_than => 1.megabytes
+  validates_attachment_content_type :featimage, :content_type => [ 'image/jpeg', 'image/png', 'image/pjpeg', 'image/gif','image/png' ]
+
+  before_post_process :unique_file_name
+    
   private
     def unique_file_name
-       extension = File.extname(picture_file_name).downcase
-       self.picture.instance_write(:file_name, "#{ActiveSupport::SecureRandom.hex(32)}#{extension}")
+       extension = File.extname(featimage_file_name).downcase
+       self.featimage.instance_write(:file_name, "#{ActiveSupport::SecureRandom.hex(32)}#{extension}")
      end
   
 end
