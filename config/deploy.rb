04a04157823c1871b27480d7210af6a7281a361e
David Li <taweili@yahoo.com>
2011-09-02 18:35:26 +0800

    restart passenger properly

--- config/deploy.rb
+++ config/deploy.rb
@@ -29,10 +29,10 @@ require "bundler/capistrano"
 # these http://github.com/rails/irs_process_scripts
 
 # If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
