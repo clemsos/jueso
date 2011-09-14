f7eb840b150e33cf2fbf82b57ccbfa4b7ba986d2
Clément Renaud <clement.renaud@gmail.com>
2011-09-14 10:33:12 +0800

    delayed_job script

--- script/delayed_job.sh
+++ script/delayed_job.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-
-if ! [ -s #{pids_dir}/delayed_job.pid ]; then  
-  RAILS_ENV=production #{RAILS_APP}/script/delayed_job start  
+if ! [ -s delayed_job.pids ]; then
+  RAILS_ENV=production /app/script/delayed_job start
 fi
