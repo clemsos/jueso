f9277895f0391a785e86551dccbf4a7fbd082efb
Clément Renaud <clement.renaud@gmail.com>
2011-09-16 15:04:29 +0800

    reduce devise mailer delayfrom 5 to 1 min

--- config/initializers/delayed_devise_mailer.rb
+++ config/initializers/delayed_devise_mailer.rb
@@ -1,15 +1,15 @@
  module Devise
     module Models
         module Confirmable
-            handle_asynchronously :send_confirmation_instructions, :run_at => Proc.new { 5.minutes.from_now.in_time_zone }
+            handle_asynchronously :send_confirmation_instructions, :run_at => Proc.new { 1.minutes.from_now.in_time_zone }
         end
 
         module Recoverable
-            handle_asynchronously :send_reset_password_instructions, :run_at => Proc.new { 5.minutes.from_now.in_time_zone }
+            handle_asynchronously :send_reset_password_instructions, :run_at => Proc.new { 1.minutes.from_now.in_time_zone }
         end
 
         module Lockable
-            handle_asynchronously :send_unlock_instructions, :run_at => Proc.new { 5.minutes.from_now.in_time_zone }
+            handle_asynchronously :send_unlock_instructions, :run_at => Proc.new { 1.minutes.from_now.in_time_zone }
         end
     end
   end
